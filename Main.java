30f0fd0b2e85cdc4e5aa359914cce65f5314e93e
rytlu <kamil.rytel.kuc96@gmail.com>
2024-04-07 11:14:24 +0200

    chore: zad1

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Point punkt = new Point(4.5, 3.0);
+
+
+        System.out.println("Współrzędne punktów: ");
+        System.out.println("x = " + punkt.getX());
+        System.out.println("y = " + punkt.getY());
+    }
+}
